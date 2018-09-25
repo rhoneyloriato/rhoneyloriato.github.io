ff23a052e020895283b460f22aad66b8c91fe84e
rhoneyloriato <33238938+rhoneyloriato@users.noreply.github.com>
2018-09-25 11:55:12 -0300

    Update script.js

--- JoKenpo/script.js
+++ JoKenpo/script.js
@@ -1,30 +1,45 @@
 var player = null;
 var comp = Math.round(Math.random() *2) ;
-
+var vitoria = 0;
+var perdeu = 0;
 function init(){
-
-	if(comp == player){
-	document.getElementById("resultado").innerHTML = "Empate";
-	}
-	else if (player == 0 && comp ==1 ) {
-	document.getElementById("resultado").innerHTML = "Voce perdeu";
-	}
-	else if (player == 0 && comp == 2 ) {
-	document.getElementById("resultado").innerHTML = "Voce ganhou";
-	}
-	else if (player == 1 && comp == 0) {
-	document.getElementById("resultado").innerHTML = "Voce ganhou";
-	}
-	else if (player == 1 && comp == 2 ) {
-	document.getElementById("resultado").innerHTML = "Voce perdeu";
-	}
-	else if (player == 2 && comp == 0) {
-	document.getElementById("resultado").innerHTML = "Voce perdeu";
-	}
-	else if (player == 2 && comp == 1) {
-	document.getElementById("resultado").innerHTML = "Voce ganhou";
-	}
-	computador();
+		
+		comp = Math.round(Math.random() *2) ;
+		computador();
+		if(comp == player){
+			document.getElementById("resultado").innerHTML = "Empate";
+		}
+		else if (player == 0 && comp ==1 ) {
+			perdeu ++;
+			document.getElementById("resultado").innerHTML = "Voce perdeu";
+			document.getElementById("derrota").innerHTML = "Derrotas: " + perdeu
+		}
+		else if (player == 0 && comp == 2 ) {
+			 vitoria ++
+			document.getElementById("resultado").innerHTML = "Voce ganhou";
+			document.getElementById("vitoria").innerHTML = "Vitorias: " + vitoria;
+		}
+		else if (player == 1 && comp == 0) {
+			 vitoria ++
+			document.getElementById("resultado").innerHTML = "Voce ganhou";
+			document.getElementById("vitoria").innerHTML = "Vitorias: " + vitoria;
+		}
+		else if (player == 1 && comp == 2 ) {
+			perdeu ++;
+			document.getElementById("resultado").innerHTML = "Voce perdeu";
+			document.getElementById("derrota").innerHTML = "Derrotas: " + perdeu;
+		}
+		else if (player == 2 && comp == 0) {
+			perdeu ++;
+			document.getElementById("resultado").innerHTML = "Voce perdeu";
+			document.getElementById("derrota").innerHTML = "Derrotas: " + perdeu;
+		}
+		else if (player == 2 && comp == 1) {
+			vitoria ++
+			document.getElementById("resultado").innerHTML = "Voce ganhou";
+			document.getElementById("vitoria").innerHTML = "Vitorias: " + vitoria;
+		}
+			
 }
 
 function computador(){
@@ -32,7 +47,7 @@ function computador(){
 	if(comp == "0"){
 			document.getElementById("pedra2").style.display = 'inline-block';
 			document.getElementById("papel2").style.display = 'none';
-		document.getElementById("tesoura2").style.display = 'none';	
+			document.getElementById("tesoura2").style.display = 'none';	
 	}
 	else if(comp == "1"){
 			document.getElementById("papel2").style.display = 'inline-block';
@@ -45,11 +60,6 @@ function computador(){
 			document.getElementById("pedra2").style.display = 'none';
 	}
 }
-
-
-
-
-
 
 function selecionar(p){
 
@@ -69,5 +79,12 @@ function selecionar(p){
 		document.getElementById("papel").style.display = 'none';
 		document.getElementById("pedra").style.display = 'none';
 	}
-
+	init();
+}
+function zerar()
+{
+	perdeu  = 0;
+	vitoria = 0;
+	document.getElementById("derrota").innerHTML = "Derrotas: " + perdeu ;
+	document.getElementById("vitoria").innerHTML = "Vitorias: " + vitoria;
 }
